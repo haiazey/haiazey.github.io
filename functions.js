14dfe58f9dfc4d8cd1112943024e2427b994c6c4
zeynep.ozturk <nur.ozturk@external.yapikredi.com.tr>
2021-03-03 22:53:08 +0300

    add data

--- functions.js
+++ functions.js
@@ -8,3 +8,15 @@ const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
 const removeBtn = document.getElementById('removeBtn');
 const showBtn = document.getElementById('showBtn');
+
+const database = firebase.database();
+
+addBtn.addEventListener('click', (e) -> {
+  e.preventDefault();
+  database.ref('/students/' + studentId.value).set({
+    first_name: firstName.value,
+    last_name: lastName.value,
+    age: age.value,
+    weight: weight.value
+  })
+})
